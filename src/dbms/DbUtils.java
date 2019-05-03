a8de85ebc56bdaa28c8fb8255023eb113f2d6f73
abhishekmairpady <abhishekmairpady@gmail.com>
2019-05-03 12:35:52 +0530

    Added all files

---
+++ src/dbms/DbUtils.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dbms;
+
+import java.sql.ResultSet;
+import javax.swing.table.TableModel;
+
+/**
+ *
+ * @author Abhishek
+ */
+class DbUtils {
+
+    static TableModel resultSetToTableModel(ResultSet rs) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
---
+++ src/dbms/javaconnect.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dbms;
+
+/**
+ *
+ * @author Abhishek
+ */
+import java.sql.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+public class javaconnect {
+    Connection conn;
+    
+    public static Connection ConnectDb(){
+        try{
+            Class.forName("java.sql.Driver");
+            String path="jdbc:mysql://localhost/jdc";
+            String user="root";
+            String pass="sudeendra";
+            Connection conn=DriverManager.getConnection(path,user,pass);
+            return conn;
+        }
+        catch(Exception e){
+            JOptionPane.showMessageDialog(null, e);
+            return null;
+        } 
+        }
+    }
+    
+       
+    
+
